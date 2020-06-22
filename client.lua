31e208479eb4037488a923b5fe6b6786022466f3
Terbium <39789348+TerbSEC@users.noreply.github.com>
2020-06-22 21:52:55 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,36 +1,6 @@
-local previousHealth = GetEntityHealth(GetPlayerPed(-1))
-Citizen.CreateThread(function()
-
-    while true do
-        Citizen.Wait(1000)
-        health = GetEntityHealth(GetPlayerPed(-1))
-        if(health ~= previousHealth) then
-            TriggerServerEvent('syncpos:sync', GetEntityCoords(GetPlayerPed(-1)))
-        end
-    end
-
-end)
-
-Citizen.CreateThread(function()
-
-    while true do
-        Citizen.Wait((15) * 1000)
-        TriggerServerEvent('syncpos:sync', GetEntityCoords(GetPlayerPed(-1)))
-    end
-
-end)
-
-
-RegisterCommand('savepos', function(source, args, raw)
-    TriggerServerEvent('syncpos:manualSave')
-end)
-
-
 RegisterNetEvent('syncpos:clientspawn')
 AddEventHandler('syncpos:spawn', function(x, y, z)
-
     SetEntityCoords(GetPlayerPed(-1), x, y, z, 1, 0, 0, 1)
-
 end)
 
 RegisterNetEvent('playerSpawned')
